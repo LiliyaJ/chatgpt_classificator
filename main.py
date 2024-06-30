53103e91a245290720bc7dc1ddecb2afccfc1c65
Liliya Jeromin <zl108108@gmail.com>
2024-06-30 17:02:10 +0200

    main function is ready

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-import pandas as pd
+from helper import get_class
+from flask import make_response
+import functions_framework
+from flask import escape
 
-from flask import make_response 
-import os
+@functions_framework.http
+def main(request):
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+     # get the string variable passed by the user
+    request_json = request.get_json(silent=True)
+    request_args = request.args
 
+    if request_json and 'url' in request_json:
+        url = request_json['url']
+    elif request_args and 'url' in request_args:
+        url = request_args['url']
+    else:
+        return 'Error: No url provided', 400
 
-from openai import OpenAI
-client = OpenAI()
+    #get class
+    result = get_class(url)
+
+    #return result
+    return result
 
